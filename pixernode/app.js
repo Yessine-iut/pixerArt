1d309667c120aabf55f3c7d1f510f7a1816c7bfe
Yessine-iut <yessinebenelbey0305@gmail.com>
2022-11-15 22:15:51 +0100

    feat (api) get/post/update/delete user and pixelBoard, only get and delete have been tested

--- pixernode/app.js
+++ pixernode/app.js
@@ -1,14 +1,165 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+const server = require('http').Server(app);
+// pour les formulaires multiparts
+var multer = require('multer');
+var multerData = multer();
 
-const hostname = '127.0.0.1';
-const port = 3001;
+const mongoDBModule = require('./connectToMongoDb');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+// Pour les formulaires standards
+const bodyParser = require('body-parser');
+// pour les formulaires multiparts
+var multer = require('multer');
+var multerData = multer();
+
+// Cette ligne indique le répertoire qui contient
+// les fichiers statiques: html, css, js, images etc.
+app.use(express.static(__dirname + '/public'));
+// Paramètres standards du modyle bodyParser
+// qui sert à récupérer des paramètres reçus
+// par ex, par l'envoi d'un formulaire "standard"
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+
+	next();
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// Lance le serveur avec express
+server.listen(port);
+console.log("Serveur lancé sur le port : " + port);
+
+//------------------
+// ROUTES
+//------------------
+// Utile pour indiquer la home page, dans le cas
+// ou il ne s'agit pas de public/index.html
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/index.html');
 });
+
+
+
+// Ici des routes en :
+// http GET (pour récupérer des données)
+// http POST : pour insérer des données
+// http PUT pour modifier des données
+// http DELETE pour supprimer des données
+
+//----------------------------------------------
+// Ces routes forment l'API de l'application !!
+//----------------------------------------------
+
+// Test de la connexion à la base
+app.get('/api/connection', (req, res) => {
+	// Pour le moment on simule, mais après on devra
+	// réellement se connecte à la base de données
+	// et renvoyer une valeur pour dire si tout est ok
+	mongoDBModule.connexionMongo()
+		.then(db => {
+			let reponse;
+
+			if (!db) {
+				console.log("erreur connexion");
+				reponse = {
+					msg: "erreur de connexion err=" + err
+				}
+			} else {
+				reponse = {
+					msg: "connexion établie"
+				}
+			}
+			res.send(JSON.stringify(reponse));
+		});
+});
+
+app.get('/api/users', (req, res) => {
+	mongoDBModule.getUsers()
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+// Récupération d'un seul user par son id
+app.get('/api/user/:id', (req, res) => {
+	var id = req.params.id;
+
+	mongoDBModule.getUserById(id)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+
+app.get('/api/pixelBoards', (req, res) => {
+	mongoDBModule.getPixelBoards()
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+// Récupération d'un seul pixelBoard par son id
+app.get('/api/pixelBoard/:id', (req, res) => {
+	var id = req.params.id;
+
+	mongoDBModule.getPixelBoardById(id)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+
+// On fera l'insert par un POST, c'est le standard REST
+app.post('/api/users', multerData.fields([]), (req, res) => {
+	mongoDBModule.createUser(req.body)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+
+app.post('/api/pixelBoards', multerData.fields([]), (req, res) => {
+	mongoDBModule.createPixelBoard(req.body)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+
+// Modification d'un restaurant, on fera l'update par
+// une requête http PUT, c'est le standard REST
+app.put('/api/user/:id', multerData.fields([]), (req, res) => {
+	var id = req.params.id;
+	mongoDBModule.updateUser(id, req.body)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+
+app.put('/api/pixelBoard/:id', multerData.fields([]), (req, res) => {
+	var id = req.params.id;
+	mongoDBModule.updatePixelBoard(id, req.body)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+});
+
+// Suppression d'un restaurant
+// On fera la suppression par une requête http DELETE
+// c'est le standard REST
+app.delete('/api/user/:id', (req, res) => {
+	var id = req.params.id;
+	mongoDBModule.deleteUser(id)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+})
+app.delete('/api/pixelBoard/:id', (req, res) => {
+	var id = req.params.id;
+	mongoDBModule.deleteUser(id)
+		.then(data => {
+			res.send(JSON.stringify(data));
+		});
+})
+
+
---
+++ pixernode/connectToMongoDb.js
@@ -0,0 +1,322 @@
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+//var url = 'mongodb://localhost:27017/test';
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'pixerart';
+
+exports.connexionMongo = async () => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+console.log(db)
+	return db;
+}
+
+exports.getUsers = async () => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+  let users;
+
+	try {
+			users = await db.collection('user')
+				.find()
+        .toArray();
+        reponse = {
+          succes: true,
+          msg: "users recherchés avec succès",
+          data: users,
+        }
+		}
+	catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "erreur lors du find des users"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+};
+
+exports.getPixelBoards = async () => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+  let users;
+
+	try {
+			users = await db.collection('pixelBoard')
+				.find()
+        .toArray();
+        reponse = {
+          succes: true,
+          msg: "pixelBoard find avec succès",
+          data: users,
+        }
+		}
+	catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "erreur lors du find des pixelBoards"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+};
+
+exports.getUserById = async (id) => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+
+	try {
+		let myquery = { "_id": ObjectId(id) };
+
+		let data = await db.collection("user").findOne(myquery);
+
+		reponse = {
+			succes: true,
+			user: data,
+			error: null,
+			msg: "User trouvé"
+		};
+	} catch (err) {
+		reponse = {
+			succes: false,
+			user: null,
+			error: err,
+			msg: "User non trouvé"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+}
+exports.getPixelBoardById = async (id) => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+
+	try {
+		let myquery = { "_id": ObjectId(id) };
+
+		let data = await db.collection("pixelBoard").findOne(myquery);
+
+		reponse = {
+			succes: true,
+			user: data,
+			error: null,
+			msg: "PixelBoard trouvé"
+		};
+	} catch (err) {
+		reponse = {
+			succes: false,
+			user: null,
+			error: err,
+			msg: "PixelBoard introuvable"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+}
+
+exports.createUser = async (formData) => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+
+	try {
+		let toInsert = {
+			username: formData.username,
+			mdp: formData.mdp,
+			role: formData.role,
+      theme:formData.theme
+		};
+		await db.collection("user").insertOne(toInsert);
+		reponse = {
+			succes: true,
+			result: toInsert._id,
+			msg: "Ajout réussi " + toInsert._id
+		};
+	} catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "erreur lors du insert"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+}
+//TODO A améliorer
+exports.createPixelBoard = async (formData) => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+	try {
+		let toInsert = {
+			statut: formData.statut,
+			dateCreation: formData.dateCreation,
+      taille:formData.taille,
+			auteur: formData.auteur,
+      mode:formData.mode,
+      delai:formData.delai,
+      title:formData.title,
+      pixels:formData.pixels
+		};
+		await db.collection("pixelBoard").insertOne(toInsert);
+		reponse = {
+			succes: true,
+			result: toInsert._id,
+			msg: "Ajout réussi " + toInsert._id
+		};
+	} catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "erreur lors du insert"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+}
+
+//TODO A améliorer?
+exports.updateUser = async (id, formData) => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+
+	try {
+		let myquery = { "_id": ObjectId(id) };
+		let newvalues = {
+			$set: {
+				mdp: formData.mdp,
+				theme: formData.theme
+			}
+		};
+		let result = await db.collection("user").updateOne(myquery, newvalues);
+
+		reponse = {
+			succes: true,
+			result: result,
+			error: null,
+			msg: "Modification réussie " + result
+		};
+	} catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "Problème à la modification"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+
+}
+
+//TODO A améliorer
+exports.updatePixelBoard = async (id, formData) => {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+
+	try {
+		let myquery = { "_id": ObjectId(id) };
+		let newvalues = {
+			$set: {
+				mdp: formData.mdp,
+				theme: formData.theme
+			}
+		};
+		let result = await db.collection("user").updateOne(myquery, newvalues);
+
+		reponse = {
+			succes: true,
+			result: result,
+			error: null,
+			msg: "Modification réussie " + result
+		};
+	} catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "Problème à la modification"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+
+}
+
+exports.deleteUser = async function (id, callback) {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+
+	try {
+		let myquery = { "_id": ObjectId(id) };
+
+		let result = await db.collection("user")
+			.deleteOne(myquery);
+		reponse = {
+			succes: true,
+			result: result,
+			error: null,
+			msg: "Suppression réussie " + result
+		};
+
+	} catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "Problème à la suppression"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+}
+
+exports.deletePixelBoard = async function (id, callback) {
+	let client = await MongoClient.connect(url, { useNewUrlParser: true });
+	let db = client.db(dbName);
+	let reponse;
+
+	try {
+		let myquery = { "_id": ObjectId(id) };
+
+		let result = await db.collection("pixelBoard")
+			.deleteOne(myquery);
+		reponse = {
+			succes: true,
+			result: result,
+			error: null,
+			msg: "Suppression réussie " + result
+		};
+
+	} catch (err) {
+		reponse = {
+			succes: false,
+			error: err,
+			msg: "Problème à la suppression"
+		};
+	} finally {
+		client.close();
+		return reponse;
+	}
+}
+
