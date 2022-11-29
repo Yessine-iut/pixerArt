4b47ec6e363fa89f49dfdc7acd7e94a02ad32175
Hugo-Nortier <hugo.nortier@etu.unice.fr>
2022-11-29 21:14:51 +0100

    feat(front) #12 viewing how many pixelboards where created in home page

--- pixerart/src/pages/Home.js
+++ pixerart/src/pages/Home.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 import { Col, Row } from 'reactstrap';
-import {NavPixer} from '../components/NavPixer';
+import { NavPixer } from '../components/NavPixer';
+import axios from 'axios';
 
 
 export const Home = () => {
+	const api = 'http://localhost:8080/api/';
+	const [nbPixelBoards, setNbPixelBoards] = useState(-1);
+
+	axios.get(api+'pixelBoards/').then(resp => {
+		setNbPixelBoards(resp.data.data.length);
+    }).catch(err => {
+        console.log(err);
+    })
+
 	return (
 		<React.StrictMode>
-			<NavPixer/>
+			<NavPixer />
 			<Row>
 				<Col>
 					<h1>Home</h1>
 					<Link to="/profile">Votre profile</Link>
+					<p>Nombre de Pixelboards: {nbPixelBoards}</p>
 				</Col>
 			</Row>
 		</React.StrictMode>
